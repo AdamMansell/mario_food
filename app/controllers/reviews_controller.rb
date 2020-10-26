850a82fd7349c3f24385dcf01f26b9fb007d883b
AdamManselll <adammansellhmb@gmail.com>
2020-10-25 21:29:00 -0700

    Add product reviews creation functionality

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,27 @@
 class ReviewsController < ApplicationController
   def new
     @product = Product.find(params[:product_id])
-    @review = @product.reviews.new
-    render :new
+    @review = @product.reviews.new    
+  end
+
+  def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.new(review_params)
+    if @review.save
+      redirect_to product_path(@product), notice: 'Successfuly created review!'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.find(params[:id])
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:author, :content_body, :rating)
   end
 end
