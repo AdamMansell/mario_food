b870da1e281b9508d08bbcb98ee7a86b26a3ffd3
AdamManselll <adammansellhmb@gmail.com>
2020-10-25 21:28:08 -0700

    Products CRUD actions are working

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,19 +1,18 @@
 class ProductsController < ApplicationController
-
   def index
     # Code for listing all products goes here.
     @products = Product.all
-    render :index
+    @most_recent_products = Product.order(created_at: :desc).limit(3)
   end
 
   def new
     # Code for new product form goes here.
     @product = Product.new
-    render :new
   end
 
   def create
     # Code for creating a new product goes here.
+    # binding.pry
     @product = Product.new(product_params)
     if @product.save
       redirect_to products_path
@@ -25,12 +24,11 @@ class ProductsController < ApplicationController
   def edit
     # Code for editing a product form goes here.
     @product = Product.find(params[:id])
-    render :edit
   end
 
   def show
     # Code for showing a single product goes here.
-    @product = Product.find(params[:id])    
+    @product = Product.find(params[:id])
   end
 
   def update
@@ -47,12 +45,13 @@ class ProductsController < ApplicationController
     # Code for deleting a product goes here.
     @product = Product.find(params[:id])
     @product.destroy
-    redirect_to product_path
+    redirect_to products_path
   end
 
   private
   
     def product_params
+      # strong params, whitelisting attributes to be saved to the database
       params.require(:product).permit(:name, :cost, :country_of_origin)
     end
 end
