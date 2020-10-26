4b8a6319562534823274546ae61dd9d04c305e97
AdamManselll <adammansellhmb@gmail.com>
2020-10-25 21:28:37 -0700

    add avg rating method for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,11 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
+
+  def avg_rating
+    if reviews.any?
+      reviews.sum(:rating) / reviews.count
+    else
+      0.0
+    end  
+  end
 end
