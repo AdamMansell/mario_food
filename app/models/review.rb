7e2f4f165d1dd3c729974d2af436f9b59c5412d3
AdamManselll <adammansellhmb@gmail.com>
2020-10-26 19:27:37 -0700

    Add validation for content body length

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,6 @@ class Review < ApplicationRecord
   belongs_to :product
 
   validates :author, :content_body, :rating, presence: true  
-  validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }  
+  validates :content_body, length: { minimum: 50, maximum: 500 }  
 end
