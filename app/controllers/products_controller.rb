98853405636d23151a6a19258d3d68e04f48a72f
AdamManselll <adammansellhmb@gmail.com>
2020-10-26 18:47:15 -0700

    Add products with most reviews functionality and appearence on home page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,11 @@ class ProductsController < ApplicationController
     # Code for listing all products goes here.
     @products = Product.all
     @most_recent_products = Product.order(created_at: :desc).limit(3)
+    @products_with_most_reviews = Product.select("products.id, products.name, count(reviews.id) as reviews_count")
+                                         .joins(:reviews)
+                                         .group("products.id")
+                                         .order("reviews_count DESC")
+                                         .limit(3)
   end
 
   def new
