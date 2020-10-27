2453c88bf7ef452d2f21253cc805c21fca60e69a
AdamManselll <adammansellhmb@gmail.com>
2020-10-26 19:19:15 -0700

    Add validation for rating in reviews

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :product
+
+  validates :author, :content_body, :rating, presence: true  
+  validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
