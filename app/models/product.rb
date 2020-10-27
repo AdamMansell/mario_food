50f547cf26a03b5f089f4c17de0609661655081f
AdamManselll <adammansellhmb@gmail.com>
2020-10-26 19:06:22 -0700

    Add validations for product name, cost, and country_of_origin values

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
 
+  validates :name, :cost, :country_of_origin, presence: true
+
   def avg_rating
     if reviews.any?
       reviews.sum(:rating) / reviews.count
