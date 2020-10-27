157634641535f2358753a7da5be1d1637efda62d
AdamManselll <adammansellhmb@gmail.com>
2020-10-26 19:46:48 -0700

    Add faker seed data for 50 products and 250 reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+50.times do
+  product = Product.create!(name: Faker::Name.name, country_of_origin: Faker::Address.country, cost: Faker::Number.between(from: 100, to: 500))
+  5.times do
+    product.reviews.create!(author: Faker::Name.name, content_body: Faker::Lorem.paragraph_by_chars(number: 100, supplemental: false), rating: Faker::Number.between(from: 1, to: 5))
+  end
+end
