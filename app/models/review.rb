e391b8847843ea8914eae6ec9eaaa48170def835
AdamManselll <adammansellhmb@gmail.com>
2020-10-27 20:17:58 -0700

    Add specs for review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,9 @@
-class Review < ApplicationRecord
-  belongs_to :product  
+# frozen_string_literal: true
 
-  validates :author, :content_body, :rating, presence: true  
-  validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }  
-  validates :content_body, length: { minimum: 50, maximum: 500 }
+class Review < ApplicationRecord
+  belongs_to :product
+
+  validates :author, :content_body, :rating, presence: true
+  validates :rating, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
+  validates :content_body, length: { minimum: 50, maximum: 250 }
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,30 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe Review do
-  it { should belong_to :product }
+  describe 'associations' do
+    it { should belong_to :product }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :author }
+    it { should validate_presence_of :content_body }
+    it { should validate_presence_of :rating }
+
+    context 'rating' do
+      it do
+        should validate_numericality_of(:rating)
+          .is_greater_than_or_equal_to(1)
+          .is_less_than_or_equal_to(5)
+      end
+    end
+
+    context 'content body' do
+      it do
+        should validate_length_of(:content_body)
+          .is_at_least(50).is_at_most(250)
+      end
+    end
+  end
 end
