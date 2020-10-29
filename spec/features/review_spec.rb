3aa75509348e909918633c0e8dd2c81001725b38
AdamManselll <adammansellhmb@gmail.com>
2020-10-28 18:29:29 -0700

    Add test for displaying review

--- spec/features/review_spec.rb
+++ spec/features/review_spec.rb
@@ -16,4 +16,25 @@ describe "Add a review" do
     click_button 'Create Review'
     expect(page).to have_content "This stuff is awesome, my kids can't even tell it's dog food."
   end
+
+  describe "Show product reviews" do
+    it "displays the reviews of a product" do
+      product = Product.create!(name: 'Mango Milk Shake', country_of_origin: 'USA', cost: 10)
+      review_content_body_1 = 'Tastes just like a real mango even though it is a bunch of chemicals that have no actual mango in it at all.'
+      review_content_body_2 = 'You would expect the shake to be a nice orange color but instead it is more of a green and brown color and is quite off putting.'
+      product.reviews.create!(author: Faker::Name.name, content_body: review_content_body_1, rating: 5)
+      product.reviews.create!(author: Faker::Name.name, content_body: review_content_body_2, rating: 3)
+      
+      visit "/products/#{product.id}"
+  
+      expect(page).to have_content 'Mango Milk Shake'
+      expect(page).to have_content 'Cost: $ 10'
+      expect(page).to have_content 'Country of Origin: USA'
+      expect(page).to have_content 'Average Rating: 4'    
+      
+      expect(page).to have_content 'Here are the reviews for this product:'
+      expect(page).to have_content review_content_body_1
+      expect(page).to have_content review_content_body_2
+    end
+  end
 end
